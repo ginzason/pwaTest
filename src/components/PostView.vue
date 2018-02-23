4cd3fab3cfb889582b986eb19ae0dbfd33a179da
ginza_mac <ginza_mac@ginza-macui-MacBook-Pro.local>
2018-02-23 11:20:30 +0900

    pwa test

---
+++ src/components/PostView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="waiting">
+    
+  </div>
+</template>
+<script>
+  import data from '../data'
+  export default {
+    data () {
+      return {
+        'pictures': data.pictures
+      }
+    }
+  }
+</script>
+<style scoped>
+  .waiting {
+    padding: 10px;
+    color: #555;
+  }
+</style>
